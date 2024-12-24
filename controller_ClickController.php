b02201e9a3a2213cc5df586def98e6d2290539d7
USER <elshodatc111>
2024-12-24 23:15:41 +0500

    Save

---
+++ controller_ClickController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ClickUz;
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+//     Route::post('prepare', [ClickController::class, 'prepare']);
+//     Route::post('completed', [ClickController::class, 'complete']);
+
+class ClickController extends Controller{
+    public function prepare(Request $request){
+        $clickTransId = $request->input('Request.click_trans_id');
+        $serviceId = $request->input('Request.service_id');
+        $clickPaydocId = $request->input('Request.click_paydoc_id');
+        $merchantTransId = $request->input('Request.merchant_trans_id');
+        $amount = $request->input('Request.amount');
+        $action = $request->input('Request.action');
+        $error = $request->input('Request.error');
+        $errorNote = $request->input('Request.error_note');
+        $signTime = $request->input('Request.sign_time');
+        $signString = $request->input('Request.sign_string');
+        $secretKey = 'siznigSecretKeyingiz';
+        $generatedSignString = md5($clickTransId . $serviceId . $secretKey . $merchantTransId . $amount . $action . $signTime);
+        if ($signString !== $generatedSignString) {
+            return response()->json(['error' => -1, 'error_note' => 'Invalid sign_string']);
+        }
+        ClickUz::create([
+            'click_trans_id' => $clickTransId,
+            'merchant_trans_id' => $merchantTransId,
+            'amount' => $amount,
+            'amount_rub' => $amount,
+            'sign_time' => $signTime,
+            'situation' => $error
+        ]);
+        if ($error == 0) {
+            $response = [
+                'click_trans_id' => $clickTransId,
+                'merchant_trans_id' => $merchantTransId,
+                'merchant_prepare_id' => $merchantTransId,
+                'error' => 0,
+                'error_note' => 'Payment prepared successfully',
+            ];
+        } else {
+            $response = [
+                'click_trans_id' => $clickTransId,
+                'merchant_trans_id' => $merchantTransId,
+                'merchant_prepare_id' => $merchantTransId,
+                'error' => -9,
+                'error_note' => 'Do not find a user!!!',
+            ];
+        }
+        return response()->json($response);
+    }
+    public function complete(Request $request){
+        $clickTransId = $request->input('Request.click_trans_id');
+        $serviceId = $request->input('Request.service_id');
+        $clickPaydocId = $request->input('Request.click_paydoc_id');
+        $merchantTransId = $request->input('Request.merchant_trans_id');
+        $merchantPrepareId = $request->input('Request.merchant_prepare_id');
+        $amount = $request->input('Request.amount');
+        $action = $request->input('Request.action');
+        $error = $request->input('Request.error');
+        $errorNote = $request->input('Request.error_note');
+        $signTime = $request->input('Request.sign_time');
+        $signString = $request->input('Request.sign_string');
+        $secretKey = 'siznigSecretKeyingiz';
+        // $secretKey = env('MERCHANT_KEY'); 
+
+        $generatedSignString = md5($clickTransId . $serviceId . $secretKey . $merchantTransId . $merchantPrepareId . $amount . $action . $signTime);
+        if ($signString !== $generatedSignString) {
+            return response()->json(['error' => -1, 'error_note' => 'Invalid sign_string']);
+        }
+        if ($error == 0) {
+            ClickUz::where('click_trans_id', $clickTransId)->update(['situation' => 1, 'status' => 'success']);
+            Order::where('id', $merchantTransId)->update(['status' => 'yakunlandi']);
+            return response()->json([
+                'click_trans_id' => $clickTransId,
+                'merchant_trans_id' => $merchantTransId,
+                'merchant_confirm_id' => $merchantTransId,
+                'error' => 0,
+                'error_note' => 'Payment Success'
+            ]);
+        } else {
+            ClickUz::where('click_trans_id', $clickTransId)->update(['situation' => -9, 'status' => 'error']);
+            Order::where('id', $merchantTransId)->update(['status' => 'bekor qilingan']);
+            return response()->json([
+                'click_trans_id' => $clickTransId,
+                'merchant_trans_id' => $merchantTransId,
+                'merchant_confirm_id' => $merchantTransId,
+                'error' => -9,
+                'error_note' => 'Do not find a user!!!'
+            ]);
+        }
+    }
+}
---
+++ controller_OrderController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OrderController extends Controller{
+    public function generateClickUrl($order_id = 1, $orderTotal = 1000){
+        $serviceId = '12345';
+        $merchantId = '12345';
+        $clickUrl = "https://my.click.uz/services/pay";
+        $clickUrl .= "?service_id=$serviceId&merchant_id=$merchantId";
+        $clickUrl .= "&amount=$orderTotal&transaction_param=$order_id";
+
+        return $clickUrl;
+    }
+}
